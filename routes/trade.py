366306d26679201ed253267a19a0978a8f745e64
DIPENDRA KUMAR SHAH <74634983+invincibleRD@users.noreply.github.com>
2023-04-23 19:26:07 +0530

    added pagination feature

--- routes/trade.py
+++ routes/trade.py
@@ -1,83 +1,139 @@
-from fastapi import APIRouter,HTTPException
+from fastapi import APIRouter, HTTPException
 from datetime import datetime
 from models.trade import Trade
 from config.db import client
+from mongopush import putdata
 from schemas.trade import tradeEntity, tradesEntity
 from typing import List
+import math
 
 trade = APIRouter()
-con=client.TradingCompany.trades
+con = client.TradingCompany.trades2
 
 
-
-
-@trade.get("/trades")
-async def get_trades():
+@trade.get("/trades", response_model=List[Trade])
+async def get_trades() -> List[Trade]:
     print("all")
-    return tradesEntity(con.find())
+    trades = con.find()
+    return [Trade(**trade) for trade in trades]
 
 
-@trade.get('/trades/search={query}')
-async def search_trades(query:str):
+@trade.get('/trades/search={query}', response_model=List[Trade])
+async def search_trades(query: str) -> List[Trade]:
     query = {
         "$or": [
             {"counterparty": {"$regex": f".*{query}.*", "$options": "i"}},
-            {"instrument_id": {"$regex": f".*{query}.*", "$options": "i"}},
-            {"instrument_name": {"$regex": f".*{query}.*", "$options": "i"}},
+            {"instrumentId": {"$regex": f".*{query}.*", "$options": "i"}},
+            {"instrumentName": {"$regex": f".*{query}.*", "$options": "i"}},
             {"trader": {"$regex": f".*{query}.*", "$options": "i"}},
         ]
     }
     print("search by quary")
-    return tradesEntity(client.TradingCompany.trades.find(query))
+    trades = con.find(query)
+    return [Trade(**trade) for trade in trades]
 
 
-@trade.get("/trades/{id}")
-async def get_trade_by_id(id: str):
+@trade.get("/trades/{id}", response_model=Trade)
+async def get_trade_by_id(id: str) -> Trade:
     print("find by ID")
-    return tradesEntity(con.find({"trade_id":id}))
-    # if not restrade:
-    #     raise HTTPException(status_code=404, detail="Trade not found")
-    # return restrade
+    trade = con.find_one({"tradeId": id})
+    if trade is None:
+        raise HTTPException(
+            status_code=404, detail="Trade not found/Invalid trade ID")
+    return Trade(**trade)
 
 
-@trade.get("/tradesfiltered")
+@trade.get("/tradesfiltered", response_model=List[Trade])
 async def get_filtered_trades(assetClass: str = None, start: datetime = None, end: datetime = None,
-                      minPrice: float = None, maxPrice: float = None, tradeType: str = None):
+                              minPrice: float = None, maxPrice: float = None, tradeType: str = None,
+                              ) -> List[Trade]:
     filters = {}
     if assetClass:
-        filters["asset_class"] = assetClass
+        filters["assetClass"] = assetClass
     if start:
-        filters["trade_date_time"] = {"$gte": start}
+        filters["tradeDateTime"] = {"$gte": start}
     if end:
-        if "trade_date_time" in filters:
-            filters["trade_date_time"]["$lte"] = end
+        if "tradeDateTime" in filters:
+            filters["tradeDateTime"]["$lte"] = end
         else:
-            filters["trade_date_time"] = {"$lte": end}
+            filters["tradeDateTime"] = {"$lte": end}
     if minPrice:
-        filters["trade_details.price"] = {"$gte": minPrice}
+        filters["tradeDetails.price"] = {"$gte": minPrice}
     if maxPrice:
-        if "trade_details.price" in filters:
-            filters["trade_details.price"]["$lte"] = maxPrice
+        if "tradeDetails.price" in filters:
+            filters["tradeDetails.price"]["$lte"] = maxPrice
         else:
-            filters["trade_details.price"] = {"$lte": maxPrice}
+            filters["tradeDetails.price"] = {"$lte": maxPrice}
     if tradeType:
-        filters["trade_details.buySellIndicator"] = tradeType
+        filters["tradeDetails.buySellIndicator"] = tradeType
     print(filters)
     print("filtered data ")
-    return tradesEntity(con.find(filters))
+    trades = con.find(filters)
+    return [Trade(**trade) for trade in trades]
 
 
-@trade.get("/pagination")
-async def get_paginated_trades(limit: int = 10, skip: int = 0):
-    # tradesEntity(con.find().skip(skip).limit(limit)) 
-    res=[]
-    for i in range(0,limit,skip):
-        res.append(tradesEntity(con.find().skip(i).limit(skip)))
-    print("pagination  applied")
+@trade.get("/paginatedtrades")
+async def get_paginated_trades(page: int = 1, page_size: int = 5) -> dict:
+    res = {}
+    all_trades = con.find()
+    trades = [Trade(**trade) for trade in all_trades]
+    res["pageNumber"] = page
+    res["totalPages"]=math.ceil(trades.__len__()/page_size)
+    res["totalTrades"] = trades.__len__()
+    data=con.find().skip((page-1)*page_size).limit(page_size)
+    trades=[Trade(**trade) for trade in data]
+    if(trades=={} or page>res["totalPages"]):
+        res["trades"]="Please provide correct page number"
+    else:
+        res["trades"]=trades
     return res
+
+
+# @trade.get("/tradesfiltered")
+# async def get_filtered_trades(assetClass: str = None, start: datetime = None, end: datetime = None,
+#                       minPrice: float = None, maxPrice: float = None, tradeType: str = None,pageSize:int=0,limit:int=10):
+#     filters = {}
+#     if assetClass:
+#         filters["asset_class"] = assetClass
+#     if start:
+#         filters["tradeDateTime"] = {"$gte": start}
+#     if end:
+#         if "tradeDateTime" in filters:
+#             filters["tradeDateTime"]["$lte"] = end
+#         else:
+#             filters["tradeDateTime"] = {"$lte": end}
+#     if minPrice:
+#         filters["tradeDetails.price"] = {"$gte": minPrice}
+#     if maxPrice:
+#         if "tradeDetails.price" in filters:
+#             filters["tradeDetails.price"]["$lte"] = maxPrice
+#         else:
+#             filters["tradeDetails.price"] = {"$lte": maxPrice}
+#     if tradeType:
+#         filters["trade_details.buySellIndicator"] = tradeType
+#     print(filters)
+#     print("filtered data ")
+#     if(pageSize!=0):
+#         res=[]
+#         for i in range(0,limit):
+#             res.append(tradesEntity(con.find(filters).skip(i).limit(pageSize)))
+#             if(i+pageSize>=limit):
+#                 pageSize=limit-1
+#             i+=pageSize
+#         print("pagination  applied")
+#         return res
+#     return tradesEntity(con.find(filters).limit(limit))
+
+
+# @trade.get("/pagination")
+# async def get_paginated_trades(limit: int = 10, size: int = 0):
+#     # tradesEntity(con.find().skip(skip).limit(limit))
+#     res={}
+#     for i in range(0,limit,size):
+#         res.append(tradesEntity(con.find().skip(i).limit(size)))
+#     print("pagination  applied")
+#     return res
     # return tradesEntity(con.find(filters))
-
-
 
 
 # @trade.get("/trades/search")
@@ -93,8 +149,6 @@ async def get_paginated_trades(limit: int = 10, skip: int = 0):
 #     return tradesEntity(con.find(query))
 
 
-
-
 # @trade.get('/tradeslist')
 # async def list_trades():
 #     # print(client.TradingCompany.trades.find())
@@ -107,7 +161,6 @@ async def get_paginated_trades(limit: int = 10, skip: int = 0):
 # async def find_by_ID(id: str):
 #     print("find by ID")
 #     return tradesEntity(con.find({"trade_id":id}))
-    
 
 
 # # @trade.get('/tradeslist')
@@ -119,9 +172,8 @@ async def get_paginated_trades(limit: int = 10, skip: int = 0):
 # #         # for doc in cursor:
 # #         #     result.append(doc)
 # #     return {"trades": result}
-        
-#     # return tradesEntity(client.TradingCompany.trades.find({"trade_id": trader_id}))
 
+#     # return tradesEntity(client.TradingCompany.trades.find({"trade_id": trader_id}))
 
 
 # # @trade.post('/')
--- schemas/trade.py
+++ schemas/trade.py
@@ -1,36 +1,28 @@
 import datetime
 import json
 
-def userEntity(item) -> dict:
-    return {
-        "id": str(item["_id"]),
-        "name": item["name"],
-        "email": item["email"],
-        "password": item["password"]
-    }
-
 
 def tradesEntity(entity) -> list:
     return [tradeEntity(item) for item in entity]
 
-def tradeDetails(item)->dict:
-    return{
-        "buySellIndicator":item["buySellIndicator"],
-        "price":item["price"],
-        "quantity":item["quantity"]
+
+def tradeDetails(item) -> dict:
+    return {
+        "buySellIndicator": item["buySellIndicator"],
+        "price": item["price"],
+        "quantity": item["quantity"]
     }
+
 
 def tradeEntity(item) -> dict:
     return {
         "id": str(item["_id"]),
-        "assetClass": item["asset_class"],
+        "assetClass": item["assetClass"],
         "counterParty": item["counterparty"],
-        "instrumentId": item["instrument_id"],
-        "instrumentName": item["instrument_name"],
-        "tradeDateTime": item["trade_date_time"],
-        "tradeDetails": tradeDetails(item["trade_details"]),
-        "tradeId": item["trade_id"],
+        "instrumentId": item["instrumentId"],
+        "instrumentName": item["instrumentName"],
+        "tradeDateTime": item["tradeDateTime"],
+        "tradeDetails": tradeDetails(item["tradeDetails"]),
+        "tradeId": item["tradeId"],
         "trader": item["trader"]
     }
-
-
